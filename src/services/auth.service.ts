2f9e267469983d93366d20a4224aff8f3d7b9f4f
Mauricio Rojas <mauriciorojasanchez@gmail.com>
2026-05-28 22:49:50 -0500

    chore: added vercel config.

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,7 +2,7 @@ import { api } from './api'
 import type { Neurologist } from '../store/app.store'
 
 export async function getMe(): Promise<Neurologist> {
-  return api.get<Neurologist>('/me')
+  return api.get<Neurologist>('/auth/me')
 }
 
 export async function logout(): Promise<void> {
